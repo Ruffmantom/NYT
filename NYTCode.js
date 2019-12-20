e625e78e81fed6f591db3d4a7cacd91128e84780
Tom Ruff <ruffmantom@gmail.com>
2019-12-19 19:18:13 -0700

    first commit

---
+++ NYTCode.js
@@ -0,0 +1,13 @@
+var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=' + apiKey;
+var apiKey = 'GID2j6rLsppstHJDHpq4cG6z4RN7CWMC';
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+
+
+
+
+})
