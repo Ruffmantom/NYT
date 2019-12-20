c5e1bd204b0fff1200527efd090315de09ea8413
Tom Ruff <ruffmantom@gmail.com>
2019-12-19 20:44:31 -0700

    added url build function

--- NYTCode.js
+++ NYTCode.js
@@ -1,27 +1,42 @@
-var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=' + apiKey;
-var apiKey = 'GID2j6rLsppstHJDHpq4cG6z4RN7CWMC';
-var searchRecords = $('#article-count').val();
-var searchStartYear = $('#start-year').val();
-var searchEndYear = $('#end-year').val();
+// var apiKey = 'GID2j6rLsppstHJDHpq4cG6z4RN7CWMC';
+// var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=' + apiKey;
 
+
+function buildQueryUrl() {
+    var myKey = 'GID2j6rLsppstHJDHpq4cG6z4RN7CWMC';
+    var userSearch = $('#search-term').val();
+    var searchRecords = $('#article-count').val();
+    var searchStartYear = $('#start-year').val();
+    var searchEndYear = $('#end-year').val();
+    var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?';
+    var url = queryURL + 'q=' + userSearch + 'begin_date=' + searchStartYear + '&end_date=' + searchStartYear + '&api-key=' + myKey;
+    console.log(url);
+    return url;
+}
+
+
+function clear() {
+    $('#article-section').clear;
+
+}
 
 
 
 $('#run-search').on('click', function () {
-    event.preventDefault()
-    var userSearch = $('#search-term').val();
-    console.log(userSearch)
-    console.log(searchRecords)
+    event.preventDefault();
+    clear();
+    var url = buildQueryUrl();
+    console.log(url);
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    // }).then(function (response) {
+
+    // })
+
+
 })
 
 
 
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-// }).then(function (response) {
 
-
-
-
-// })
