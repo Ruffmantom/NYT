67c31a8bc1b0f3201a4be99ddd7c1fc3fce1fbd6
Tom Ruff <ruffmantom@gmail.com>
2019-12-19 19:43:44 -0700

    added a few console logs

--- NYTCode.js
+++ NYTCode.js
@@ -1,13 +1,24 @@
 var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=' + apiKey;
 var apiKey = 'GID2j6rLsppstHJDHpq4cG6z4RN7CWMC';
-
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
+var searchRecords = $('#article-count').val();
 
 
 
-
+$('#run-search').on('click', function () {
+    event.preventDefault()
+    var userSearch = $('#search-term').val();
+    console.log(userSearch)
+    console.log(searchRecords)
 })
+
+
+
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function (response) {
+
+
+
+
+// })
